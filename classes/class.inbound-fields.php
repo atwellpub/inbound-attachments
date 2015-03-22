7a850c19a541e1d4c9a0fbd26ff35509ff586c04
Hudson Atwell <hudson.atwell@gmail.com>
2015-03-22 16:26:53 -0500

    adding documentation for ajay

--- classes/class.inbound-fields.php
+++ classes/class.inbound-fields.php
@@ -19,7 +19,11 @@ if ( !class_exists( 'Inbound_Attachments_Fields' )) {
 		*	Loads hooks and filters selectively
 		*/
 		public static function load_hooks() {
+			/* Add new field type to dropdown */
 			add_filter( 'inboundnow_forms_settings' , array( __CLASS__ , 'extend_field_type_dropdown' ) );
+			
+			/* Add handler to process field type */
+			add_action( 'inbound_form_custom_field' , array( __CLASS__ , 'render_attachment_uploader') );
 		}
 		
 		/**
@@ -29,7 +33,19 @@ if ( !class_exists( 'Inbound_Attachments_Fields' )) {
 			$config['forms']['child']['options']['field_type']['options']['attachments'] = __("Attachments Uploader", "inbound-pro");
 			return $config;
 		}
+		
+		
+		/**
+		*  Listens for attachment uploader field type and renders it 
+		*/
+		public static function render_attachment_uploader( $field ) {
+			/* only render field if 'attachments' type is selected. */ 
+			
+			/* here are the values passed by the field that can be used to determine what kind of custom field this is */
+			print_r($field);exit;
+		}
 	}
+	
 
 	new Inbound_Attachments_Fields;
 }
