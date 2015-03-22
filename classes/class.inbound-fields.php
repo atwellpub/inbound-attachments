016d172a20a4be00100bfcc975cd9c84c7610f72
Hudson Atwell <hudson.atwell@gmail.com>
2015-03-21 19:15:56 -0500

    extending available field types

--- classes/class.inbound-fields.php
+++ classes/class.inbound-fields.php
@@ -19,10 +19,16 @@ if ( !class_exists( 'Inbound_Attachments_Fields' )) {
 		*	Loads hooks and filters selectively
 		*/
 		public static function load_hooks() {
-			
+			add_filter( 'inboundnow_forms_settings' , array( __CLASS__ , 'extend_field_type_dropdown' ) );
 		}
 		
-		
+		/**
+		*  Extends field type dataset for Inbound Forms
+		*/
+		public static function extend_field_type_dropdown( $config ) {
+			$config['forms']['child']['options']['field_type']['options']['attachments'] = __("Attachments Uploader", "inbound-pro");
+			return $config;
+		}
 	}
 
 	new Inbound_Attachments_Fields;
